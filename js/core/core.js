90fb03f04a60379c763465588ac9c172db15d2da
Ryan Flannery <ryan.flannery@gmail.com>
2015-01-16 21:25:06 -0500

    uikit: 2.16.0 -> 2.16.2

--- js/core/core.js
+++ js/core/core.js
@@ -1,4 +1,4 @@
-/*! UIkit 2.16.0 | http://www.getuikit.com | (c) 2014 YOOtheme | MIT License */
+/*! UIkit 2.16.2 | http://www.getuikit.com | (c) 2014 YOOtheme | MIT License */
 (function(core) {
 
     if (typeof define == "function" && define.amd) { // AMD
@@ -43,7 +43,7 @@
 
     var UI = {}, _UI = window.UIkit;
 
-    UI.version = '2.16.0';
+    UI.version = '2.16.2';
     UI._prefix = 'uk';
 
     UI.noConflict = function(prefix) {
